d2709878118242a2d2d0d89e33c26432a0721224
carolus <budowniczy.zamyslow@gmail.com>
2020-12-01 21:18:55 +0000

    implemented repr methods for orm models

--- db/models.py
+++ db/models.py
@@ -1,5 +1,6 @@
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, Date, Interval, ForeignKey, JSON
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
@@ -10,11 +11,17 @@ class User(Base):
     state_id = Column(Integer, ForeignKey('state.id'), default=None)
     session_data = Column(JSON, nullable=False)
 
+    def __repr__(self) -> str:
+        return f"<User(id={self.id}, state_id={self.state_id}, session_data={self.session_data})>"
+
 
 class State(Base):
     __tablename__ = 'state'
     id = Column(Integer, primary_key=True)
     name = Column(String, unique=True, nullable=False)
+
+    def __repr__(self) -> str:
+        return f"<State(id={self.id}, name={self.name})>"
 
 
 class Medication(Base):
@@ -22,6 +29,9 @@ class Medication(Base):
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
     name = Column(String, unique=True, nullable=False)
+
+    def __repr__(self) -> str:
+        return f"<Medication(id={self.id}, user_id={self.user_id}, name={self.name})>"
 
 
 class Prescription(Base):
@@ -35,11 +45,19 @@ class Prescription(Base):
     event_id = Column(Integer, ForeignKey('event.id'), nullable=False)
     time_delta = Column(Interval)
 
+    def __repr__(self) -> str:
+        return f"<Prescription(id={self.id}, user_id={self.user_id}, medication_id={self.medication_id}, " \
+               f"start_date={self.start_date}, end_date={self.end_date}, dose={self.dose}, event_id={self.event_id}, " \
+               f"time_delta={self.time_delta})>"
+
 
 class SpecialCondition(Base):
     __tablename__ = 'special_condition'
     id = Column(Integer, primary_key=True)
     name = Column(String, unique=True, nullable=False)
+
+    def __repr__(self) -> str:
+        return f"<SpecialCondition(id={self.id}, name={self.name})>"
 
 
 class PrescriptionConditions(Base):
@@ -47,8 +65,14 @@ class PrescriptionConditions(Base):
     prescription_id = Column(Integer, ForeignKey('prescription.id'), primary_key=True, nullable=False)
     condition_id = Column(Integer, ForeignKey('special_condition.id'), primary_key=True, nullable=False)
 
+    def __repr__(self) -> str:
+        return f"<PrescriptionConditions(prescription_id={self.prescription_id}, condition_id={self.condition_id})>"
+
 
 class Event(Base):
     __tablename__ = 'event'
     id = Column(Integer, primary_key=True)
     name = Column(String, unique=True, nullable=False)
+
+    def __repr__(self) -> str:
+        return f"<Event(id={self.id}, name={self.name})>"
